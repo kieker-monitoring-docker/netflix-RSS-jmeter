dbc34dddb70adf8b51fc0832b99f684dac3f9305
Thomas Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2015-11-04 17:41:35 +0100

    Changed version retrieval as we only need a 1.XX-SNAPSHOT kind of version string.

--- updateDockerfile.sh
+++ updateDockerfile.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # Author: Thomas F. Düllmann
 # Description:
-#  This script retrieves the latest (nightly) Kieker AspectJ JAR 
+#  This script retrieves the latest (nightly/snaphot) Kieker binary version.
 #  from the oss.sonatype.org repository. 
-#  To achieve this the metadata files are read to find out the latest version 
-#  and further date/build information to pick the right (latest) file available. 
+#  To achieve this the metadata files are read to find out the latest version .
 
 # Greps the first given input ($1) with the second ($2), removes all HTML tags and removes spaces
 function stripHtml {
@@ -19,22 +18,10 @@ METADATA="$BASE_URL/maven-metadata.xml"
 curl -s $METADATA > $TMP_FILE_1
 LATEST_VERSION_SNAPSHOT=`stripHtml $TMP_FILE_1 "latest"`
 
-# Find out the name of the latest nightly build aspectj JAR file
-TMP_FILE_2=`mktemp`
-SUB_METADATA="$BASE_URL/$LATEST_VERSION_SNAPSHOT/maven-metadata.xml"
-curl -s $SUB_METADATA > $TMP_FILE_2
-LATEST_VERSION=`stripHtml $TMP_FILE_2 "version" | cut -d "-" -f 1`
-TIMESTAMP=`stripHtml $TMP_FILE_2 "timestamp"`
-BUILD_NUMBER=`stripHtml $TMP_FILE_2 "buildNumber"`
-
-FILE_NAME="kieker-$LATEST_VERSION-$TIMESTAMP-$BUILD_NUMBER-aspectj.jar"
-
 # Housekeeping
 rm $TMP_FILE_1
-rm $TMP_FILE_2
 
 if [ -f Dockerfile ]; then
-  sed -i "s%KIEKER_VERSION .*%KIEKER_VERSION $LATEST_VERSION-$TIMESTAMP-$BUILD_NUMBER%g" Dockerfile
-  sed -i "s%KIEKER_AGENT_BASE_URL .*%KIEKER_AGENT_BASE_URL \"$BASE_URL/$LATEST_VERSION_SNAPSHOT\"%g" Dockerfile
+  sed -i "s%KIEKER_VERSION .*%KIEKER_VERSION $LATEST_VERSION_SNAPSHOT%g" Dockerfile
 fi
 
